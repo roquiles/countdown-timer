2100d7b548f43b5db7b12668d9706f0daeda2fc3
Roberta Quiles <roberta_btg@hotmail.com>
2022-01-03 23:27:42 -0300

    Style the modal window

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const btnOpenModal = document.querySelectorAll(".btn-open-modal");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+// MODAL WINDOW
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+btnOpenModal.forEach((element) => {
+  element.addEventListener("click", openModal);
+});
