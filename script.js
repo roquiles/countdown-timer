f147fecccf69f3cf72f7d0d0d42aae4403436346
Roberta Quiles <roberta_btg@hotmail.com>
2022-01-04 15:31:31 -0300

    Respond the ESC keydown event on modal window

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const btnsOpenCloseModal = document.querySelectorAll(".btn-modal");
-const btnCloseModal = document.getElementById("btn-close-modal");
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 
@@ -15,4 +14,11 @@ const openCloseModal = function (e) {
 
 btnsOpenCloseModal.forEach((element) => {
   element.addEventListener("click", openCloseModal);
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  }
 });
